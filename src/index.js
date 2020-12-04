7ccd8c46f19cdacada9021e9518f6a06b31bcde4
Kirilov Vladislav <kirilov.vl.ol@gmail.com>
2020-12-04 09:21:28 +0300

    исправлен hash router для gh pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 // HashRouter для gh pages
-// "homepage": "https://KirilovVladislav.github.io/test-avito",
 import { HashRouter, BrowserRouter } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -14,7 +13,7 @@ import store from './store'
 
 ReactDOM.render(
   // <BrowserRouter>
-  <HashRouter basename={`https://KirilovVladislav.github.io/test-avito`}>
+  <HashRouter basename={`/`}>
     <Provider store={store}>
       <App />
     </Provider>
